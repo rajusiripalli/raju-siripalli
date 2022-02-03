b7cbdc622ddd71066d82aba4744723416ec94310
Raju Dhoni <32018260+rajusiripalli@users.noreply.github.com>
2022-02-03 18:50:02 +0530

    header portfolio contianer utilities updated

--- src/PortfolioContainer/Home/Home.js
+++ src/PortfolioContainer/Home/Home.js
@@ -2,11 +2,14 @@ import React from 'react';
 import './Home.css'
 import Footer from './Footer/Footer';
 import Profile from './Profile/Profile';
+import Header from './Header/Header';
+
 
 
 export default function Home() {
   return (
       <div className='home-container'>
+          <Header />
           <Profile />
           <Footer />
       </div>
